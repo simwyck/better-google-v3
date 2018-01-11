abc0465ae9c1ac4b4234f7db8f57ec446a3d9ac3
Simon Wyckaert <simon@digitold.com>
2018-01-11 21:43:52 +0100

    nettoyage du code

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ THP - J4
 La page d'accueil de Google pue ! On a donc décidé de la refaire mais en beaucoup mieux, pour leur montrer ce que c'est que des vrais pros du CSS et HTML.
 
 A voir en live sur : http://better-google-v2.surge.sh/
-(N'oublie pas d'autoriser les popips dans ton navigateur.)
+
+(N'oublie pas d'autoriser les popups dans ton navigateur.)
 
  - Simon (@simwyck)
--- scripts/main.js
+++ scripts/main.js
@@ -3,38 +3,14 @@ function afficherElementsCentraux() {
     document.getElementById("cestmagique").style.display='block';
 }
 
-/* Faire une recherche Google avec les mot-clés saisis par l'utilisateur */
-
-/* let monBouton = document.querySelector('bouton-recherche');
-let maRecherche = document.querySelector('recherche');
-
-function saisirMotsCles() {
-  let maRecherche = prompt('Veuillez saisir votre nom.');
-  localStorage.setItem('rechercheuser', maRecherche);
-  monTitre.textContent = 'Mozilla est cool, ' + maRecherche;
-}
-
-function saisirMotsCles() {
-    document.getElementById("formrech").submit();
-} */
-
-/* On récupère la requête et on affiche la page de résultat. Mais bon, ça buggue pour l'instant. */
+/* On récupère la requête utilisateur et on affiche la page de résultat. Mais bon, ça buggue pour l'instant. */
 function rechercherSurGoogle() {
     let maRecherche = document.formrech.recherchebox.value;
-    /* alert(maRecherche); */
-    /* let adresseGoogle = "https://www.google.fr/#q="; */
     let requeteUser = "https://www.google.fr/#q=" + maRecherche;
+    /* J'arrive bien à afficher l'alert mais pas à lancer l'affichage de la page */
     /* alert(requeteUser); */
-    /* localStorage.setItem('rechercheuser', maRecherche);
-    self.location='https://www.google.fr/#q= + maRecherche'; */
     document.location.href = 'requeteUser';
 }
-
-/* <form>
-<input type=button
-value="insert button text here"
-onClick="self.location='Your_URL_here.htm'">
-</form> */
 
 /* Afficher un popup après un délai */
 function afficherPopup() {
